e8740d7b829e01856c8a6a8f5cf9c7adc5dec0ff
Kasania <nasang97@naver.com>
2018-05-08 00:00:33 +0900

    override operator=

--- template/main.cpp
+++ template/main.cpp
@@ -2,8 +2,12 @@
 #include "myvector.h"
 
 auto main() -> int {
-	vector<int> v;
-	//v.setSize(10);
-	std::cout << v.size() << std::endl;
+	vector<int> v1;
+	std::cout << v1.cap() << std::endl;
+	vector<int> v2(15);
+	std::cout << v2.cap() << std::endl;
+	v1 = v2;
+	std::cout << v1.cap() << std::endl;
+	std::cout << v2.cap() << std::endl;
 	return 0;
 }
--- template/myvector.cpp
+++ template/myvector.cpp
@@ -1,29 +1,103 @@
+/*	
+	vector class
+
+*/
+#include <iostream>
 
 template <typename T>
 class vector {
 public :
-	vector(const size_t size = defaultSize)
+	static const size_t defaultCap = 10;
+
+	vector(vector&& v) {
+		this->curCap = v.cap;
+		this->curSize = v.size;
+		this->elements = v.elements;
+	}
+
+	vector(const size_t cap = defaultCap)
 	{
-		curSize = size;
+		curCap = cap;
+		elements = new T[curCap];
+		curSize = 0;
 	}
 	
 	~vector()
 	{
-
+		delete[] elements;
+		elements = nullptr;
 	}
-	
-	void setSize(const size_t size)
+
+	const bool reserve(const size_t cap)
 	{
 		curSize = size;
+		//todo : moveElements and extends Vector
+		return true;
 	}
 	
 	const size_t& size() const
 	{
 		return curSize;
 	}
-	static const size_t defaultSize = 10;
+
+	const size_t& cap() const {
+		return curCap;
+	}
+
+	const bool insert(const T& target, const int pos) {
+		//todo : insert method
+		return true;
+	}
+
+	const bool move(vector& v){
+		this = v;
+		return true;
+	}
+	
+	//copy
+	vector<T>& operator = (vector& v) {
+		if (this == &v) return *this;
+		delete[] elements;
+		this->elements = new T[curCap];
+		copyElements(v.elements,v.curSize);
+		this->curSize = v.curSize;
+		this->curCap = v.curCap;
+		std::cout << "copied" << std::endl;
+		return *this;
+	}
+	//move
+	vector<T>& operator = (vector&& v) {
+		if (this == &v) return *this;
+		//delete[] elements;
+		this->curSize = v.curSize;
+		this->curCap = v.curCap;
+		this->elements = v.elements;
+		v.curSize = 0;
+		v.curCap = 0;
+		v.elements = nullptr;
+		std::cout << "moved" << std::endl;
+		return *this;
+	}
 
 private :
+	
 	size_t curSize;
+	size_t curCap;
+	T* elements;
+
+	const bool isInBound(const int pos) {
+		if (pos < curCap) return true;
+		return false;
+	}
+
+	const bool copyElements(const T* src,const size_t len) {
+
+		for (auto i = 0; i < len; ++i) {
+			elements[i] = src[i];
+		}
+		return true;
+	}
+
 
 };
+
